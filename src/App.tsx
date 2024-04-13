a99880bf1209dac215ddeba9107220e4c6492e3d
Adriana Sobota <universe7raindrop@gmail.com>
2024-04-13 22:14:36 +0200

    not found page

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         <Route path="/about" element={<AboutPage/>} />
         <Route path="/contact" element={<ContactPage/>} />
         <Route path="/how-to-buy" element={<HowToBuy/>} />
-        <Route element={<NotFoundPage/>} />
+        <Route  path="/*" element={<NotFoundPage/>} />
       </Routes>
     </Router>
   );
