cd143fff470db925af3a36263050a65d4016d9ff
Adriana Sobota <universe7raindrop@gmail.com>
2024-04-28 23:14:39 +0200

    api url update full

--- src/services/contactusapi.ts
+++ src/services/contactusapi.ts
@@ -4,21 +4,22 @@ interface FormData {
   message: string;
 }
 
-interface Configurations {
-  apiUrl: string;
-}
+// interface Configurations {
+//   apiUrl: string;
+// }
 
 // Type guard to check if window.configs is of type Configurations
-function hasConfigurations(obj: any): obj is { configs: Configurations } {
-  return obj && obj.configs && typeof obj.configs.apiUrl === "string";
-}
+// function hasConfigurations(obj: any): obj is { configs: Configurations } {
+//   return obj && obj.configs && typeof obj.configs.apiUrl === "string";
+// }
 
 // Get the API URL from the window.configs, with a fallback to "/"
-const apiUrl = hasConfigurations(window) ? window.configs.apiUrl : "/";
+// const apiUrl = hasConfigurations(window) ? window.configs.apiUrl : "/";
+
 
 export async function submitFormData(formData: FormData): Promise<boolean> {
   // Here we use the `apiUrl` obtained through the type guard above
-  const endpoint = `${apiUrl}0f55ae80-8bdd-4d91-9fbf-a7b5db1be7f3-prod.e1-us-cdp-2.choreoapis.dev/impreza/impreza-back-end/simple-image-and-form-submission-api-197/v1.0/api/submit-form`;
+  const endpoint = `https://0f55ae80-8bdd-4d91-9fbf-a7b5db1be7f3-prod.e1-us-cdp-2.choreoapis.dev/impreza/impreza-back-end/simple-image-and-form-submission-api-197/v1.0/api/submit-form`;
   console.log(endpoint);
   try {
     const response = await fetch(endpoint, {
