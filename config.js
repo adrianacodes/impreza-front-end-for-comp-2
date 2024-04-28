e1c6d0920fed199cd60a0c8aacd9bbe4e1e0c922
Adriana Sobota <universe7raindrop@gmail.com>
2024-04-28 21:16:19 +0200

    backend update withought axios

---
+++ config.js
@@ -0,0 +1,3 @@
+window.configs = {
+    apiUrl: '/choreo-apis/impreza/impreza-back-end/simple-image-and-form-submission-api-197/v1.0',        
+};
--- src/services/contactusapi.ts
+++ src/services/contactusapi.ts
@@ -4,9 +4,24 @@ interface FormData {
   message: string;
 }
 
+interface Configurations {
+  apiUrl: string;
+}
+
+// Type guard to check if window.configs is of type Configurations
+function hasConfigurations(obj: any): obj is { configs: Configurations } {
+  return obj && obj.configs && typeof obj.configs.apiUrl === 'string';
+}
+
+// Get the API URL from the window.configs, with a fallback to "/"
+const apiUrl = hasConfigurations(window) ? window.configs.apiUrl : "/";
+
 export async function submitFormData(formData: FormData): Promise<boolean> {
+  // Here we use the `apiUrl` obtained through the type guard above
+  const endpoint = `${apiUrl}/impreza-back-end/simple-image-and-form-submission-api-197/v1.0`;
+
   try {
-    const response = await fetch("https://0f55ae80-8bdd-4d91-9fbf-a7b5db1be7f3-prod.e1-us-cdp-2.choreoapis.dev/impreza/impreza-back-end/simple-image-and-form-submission-api-197/v1.0", {
+    const response = await fetch(endpoint, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
